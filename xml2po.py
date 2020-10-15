a910984ecf3a879333b2791c6705c886c7a8b031
Persian Prince <persianpros@yahoo.com>
2020-10-15 09:09:00 +0330

    Update xml2po.py

--- xml2po.py
+++ xml2po.py
@@ -3,13 +3,14 @@
 from __future__ import print_function
 import sys
 import os
-import string
+import six
+import re
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler, property_lexical_handler
 try:
 	from _xmlplus.sax.saxlib import LexicalHandler
 	no_comments = False
-except ImportError as e:
+except ImportError:
 	class LexicalHandler:
 		pass
 	no_comments = True
@@ -19,16 +20,19 @@ class parseXML(ContentHandler, LexicalHandler):
 		self.isPointsElement, self.isReboundsElement = 0, 0
 		self.attrlist = attrlist
 		self.last_comment = None
+		self.ishex = re.compile('#[0-9a-fA-F]+\Z')
 
 	def comment(self, comment):
-		if comment.find("TRANSLATORS:") != -1:
+		if "TRANSLATORS:" in comment:
 			self.last_comment = comment
 
 	def startElement(self, name, attrs):
-		for x in ["text", "title", "value", "caption"]:
+		for x in ["text", "title", "value", "caption", "summary", "description"]:
 			try:
-				attrlist.add((attrs[x], self.last_comment))
-				self.last_comment = None
+				k = six.ensure_str(attrs[x])
+				if k.strip() != "" and not self.ishex.match(k):
+					attrlist.add((attrs[x], self.last_comment))
+					self.last_comment = None
 			except KeyError:
 				pass
 
@@ -52,15 +56,14 @@ for arg in sys.argv[1:]:
 	attrlist = list(attrlist)
 	attrlist.sort(key=lambda a: a[0])
 
-	for (k,c) in attrlist:
-		print
+	for (k, c) in attrlist:
+		print()
 		print('#: ' + arg)
-		string.replace(k, "\\n", "\"\n\"")
+		k.replace("\\n", "\"\n\"")
 		if c:
 			for l in c.split('\n'):
 				print("#. ", l)
-		if str(k).strip() != "":
-			print('msgid "' + str(k) + '"')
-			print('msgstr ""')
+		print('msgid "' + six.ensure_str(k) + '"')
+		print('msgstr ""')
 
 	attrlist = set()
